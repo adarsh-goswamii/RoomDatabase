7dc9b17431ae93d3b974c5168f9884cd10f14028
Adarsh Goswami <adarsh010120@gmail.com>
2021-05-11 21:42:25 +0530

    Singleton pattern added

--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.roomdatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UniversityDatabase db= UniversityDatabase.getInstance(this);
+        List<Student> studentList= db.studentDao().getAllStudents();
+
+        Log.e("database", studentList.get(0).getName()+ studentList.get(2).getName());
     }
 }
--- app/src/main/java/com/example/roomdatabase/UniversityDatabase.java
+++ app/src/main/java/com/example/roomdatabase/UniversityDatabase.java
@@ -1,6 +1,13 @@
 package com.example.roomdatabase;
 
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
 import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 /**
  * There are few things which are imp as we defining UniversityDatabase
@@ -8,7 +15,6 @@ import androidx.room.Database;
  * 2. It should extend RoomDatabase
  * 3. For every Dao interface that has been created we need to specify one function for it in the
  * university Database.
- *
  */
 
 /**
@@ -18,11 +24,71 @@ import androidx.room.Database;
  * 2. The version of our database, as in future with app updates our database can be updated as well.
  */
 @Database(entities = Student.class, version = 1)
-public abstract class UniversityDatabase {
+public abstract class UniversityDatabase extends RoomDatabase {
     public abstract StudentDao studentDao();
 
     /**
      * Making instances of room database is quite heavy that's why we need to ensure that there is just
-     * one instance of University Database for that reason we will use singleton.
+     * one instance of University Database for that reason we will use singleton pattern.
      */
+
+    private static UniversityDatabase instance;
+
+    public static synchronized UniversityDatabase getInstance(Context context) {
+        if (instance == null) {
+            /**
+             * we need to create a instance if its null database builder takes 3 parameters
+             * 1. context
+             * 2. class which extends RoomDatabase
+             * 3. Name of the database
+             *
+             * .fallbackToDestructiveMigration() : So in sqlite we had onUpgrade and downgrade for updating the database but in room
+             * database this is handled using migration. Here we want to create our database from scratch that's why we mention
+             * fallbackToDestructiveMigration().
+             *
+             * .allowMainThreadQueries() : So generally it is advised to create or make queries from database in some other thread other
+             * than main but here we have very small database and for simplicity we will do it in main thread only so that's why we add
+             * allowMainThreadQueries().
+             *
+             * .addCallback() : So we want to add some initial values to our database that's why we use call back.
+             *
+             * we have created a diff thread and in which we will be adding three student to our table.
+             */
+
+            instance = Room.databaseBuilder(context, UniversityDatabase.class, "university_db")
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .addCallback(initialCallback)
+                    .build();
+        }
+        return instance;
+    }
+
+    public static RoomDatabase.Callback initialCallback = new RoomDatabase.Callback() {
+
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            new PopulateInitialData(instance).execute();
+        }
+    };
+
+    private static class PopulateInitialData extends AsyncTask<Void, Void, Void> {
+
+        private StudentDao studentDao;
+
+        public PopulateInitialData(UniversityDatabase db) {
+            this.studentDao = db.studentDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            studentDao.addSingleStudent(new Student("email1", "num1", "Name1"));
+            studentDao.addSingleStudent(new Student("email2", "num2", "Name2"));
+            studentDao.addSingleStudent(new Student("email3", "num3", "Name3"));
+
+            return null;
+        }
+    }
 }
